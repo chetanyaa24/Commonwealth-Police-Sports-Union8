ef71bd271997363e678322578ef98b8211739c02
Chetanya Sachdeva <166475607+chetanyaa24@users.noreply.github.com>
2025-01-30 14:40:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const teamMembers = [
     { name: "Rajendra Prasad Sharma (IPS: 1986)", description: "Former Director General of Police, Odisha", image: "images/person5.png" },
     { name: "Ram Prawesh Thakur (IPS: 1986)", description: "Former Director General of Police, Andhra Pradesh", image: "images/person6.png" },
     { name: "R Chandranathan (IPS: 1986)", description: "Former Director General of Police, Meghalaya", image: "images/person7.png" },
-    { name: "Vinod Kumar Mall (IPS: 1986)", description: "Former Director General of Police, Gujarat & President,Police & Fire Games Board of India", image: "images/person8.jpg" },
+    { name: "Vinod Kumar Mall (IPS: 1986)", description: "Former Director General of Police, Gujarat ", image: "images/person8.jpg" },
     { name: "Sunil Kumar Singh (IPS: 1988)", description: "Former Director General of Police, Tamil Nadu", image: "images/person9.jpg" },
     { name: "Sanjay Kundu (IPS: 1989)", description: "Former Director General of Police, Himachal Pradesh", image: "images/person10.jpg" },
     { name: "Umesh Mishra (IPS: 1989)", description: "Former Director General of Police, Rajasthan", image: "images/person11.jpg" },
@@ -20,10 +20,11 @@ const teamMembers = [
     { name: "Mrs Anuradha Upadhyay", description: "Senior Advocate Rajasthan High Court, Jaipur Bench", image: "images/person18.jpg" },
     { name: "Abdul Salam Sankhala", description: "Senior Advocate, District & Session Court, Jaipur", image: "images/person19.jpg" },
     { name: "Brij Raj Singh Shekhawat", description: "Trustee, Prehelika Foundation, India", image: "images/person20.jpg" },
-    { name: "Jogender Singh Salaria", description: "Founder of PCT Humanity, member of Dubai Humanitarian, UAE, Businessman & Global Social Worker, Golden Visa Holder", image: "images/person21.png" },
+    { name: "Sunil Kumar", description: "Secretary, Police & Fire Games Board Of India", image: "images/SUNIL_KUMAR3.png" },
     { name: "Tanmay Bathwal", description: "Managing Director, Hirise Hospitality Pvt Ltd", image: "images/person22.png" },
     { name: "Mrs. Prisha S Bakshi", description: "Renowned Entrepreneur & Finance Expert, New Delhi", image: "images/person23.jpg" },
-    { name: "Sunil Kumar",description: "SECRETARY, POLICE & FIRE GAMES BOARD OF INDIA", image: "images/SUNIL_KUMAR3.png" }
+    { name: "Colnel Rupinderjit Hayer (R)", description: "Canada", image: "images/person24.jpg" },
+    { name: "Jogender Singh Salaria", description: "Founder of PCT Humanity, member of Dubai Humanitarian, UAE, Businessman & Global Social Worker, Golden Visa Holder", image: "images/person21.png" }
 ];
 
 // Reference to the team container
@@ -60,7 +61,7 @@ window.addEventListener('load', () => {
         if (mainContent) {
             mainContent.style.display = 'block'; // Show main content
         }
-    }, 1500); 
+    }, 1500);
 });
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
